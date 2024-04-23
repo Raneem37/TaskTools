6c1207f5edcb5e70f210a1f1ac2c209ba165634d
Raneem37 <150626304+Raneem37@users.noreply.github.com>
2024-04-23 16:39:15 +0200

    First Commit

---
+++ src/main/java/Task/CalResult.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class CalResult {
+	public CalResult(double r){
+		Result =r;
+	}
+	private double Result;
+	public double getResult() {
+		return Result;
+	}
+	public void setResult(double r) {
+		Result =r;
+	}
+}
